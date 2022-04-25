3d6b5552db84e3c33100e83578628388d2d697c0
James Bryant <jamesbryant792@gmail.com>
2022-04-25 12:09:45 +0100

    updated transform details app

--- opendata-transform-details-lambda/app.py
+++ opendata-transform-details-lambda/app.py
@@ -10,7 +10,7 @@ from lambdautils.helpers import (
     dataset_name_from_zip_name,
 )
 from lambdautils.mocking import get_lambda_client
-from lambdautils.schemas import source_bucket_schema, transform_details_schema
+from lambdautils.schemas import transform_details_event_schema, metadata_validator_response_schema
 
 import logging
 
@@ -25,16 +25,14 @@ def handler(event, context):
     Returns transform_details
     """
 
+    json_validate(event, transform_details_event_schema) 
+
     client = get_lambda_client()
 
-    json_validate(event, source_bucket_schema)
-
-    if isinstance(event, str):
-        event = json.loads(event)
     zip_file = event["zip_file"]
     dataset_name = dataset_name_from_zip_name(zip_file)
 
-    # validate metadata
+    # validate metadata by calling the metadata validator
     r = client.invoke(
         FunctionName="opendata-metadata-validator",
         InvocationType="RequestResponse",
@@ -42,6 +40,7 @@ def handler(event, context):
     )
 
     payload_dict = json.load(r["Payload"])
+    json_validate(payload_dict, metadata_validator_response_schema)
     if payload_dict["statusCode"] != 200:
         log_as_incomplete()
         raise Exception(
@@ -49,7 +48,6 @@ def handler(event, context):
         )
 
     is_valid = payload_dict["body"]["valid"]
-    assert isinstance(is_valid, bool)
     if not is_valid:
         log_as_incomplete()
         raise Exception("Aborting for invalid metadata.")    
@@ -59,10 +57,10 @@ def handler(event, context):
         data = json.load(f)
 
     transform_details_dict = data[dataset_name]
-    json_validate(transform_details_dict, transform_details_schema)
 
     log_as_complete()
     return {
         "statusCode": 200,
-        "body": {"transform_details": transform_details_dict}
+        "body": {"transform_details": transform_details_dict},
+        "request_id": event["request_id"]
     }
