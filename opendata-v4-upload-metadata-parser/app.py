8878dab0c9332531eaea9f09693b1cbf35329240
James Bryant <jamesbryant792@gmail.com>
2022-04-25 12:11:52 +0100

    updated metadata parser app

--- opendata-v4-upload-metadata-parser/app.py
+++ opendata-v4-upload-metadata-parser/app.py
@@ -9,9 +9,11 @@ from lambdautils.helpers import (
     Source,
     MetadataHandler,
     COMMON_ZIP_PATH,
+    empty_tmp_folder,
+    cmd_csvw_metadata_parser,
 )
 from lambdautils.mocking import get_s3_client
-from lambdautils.schemas import source_bucket_schema, valid_metadata_schema
+from lambdautils.schemas import upload_metadata_parser_event_schema
 
 
 def handler(event, context):
@@ -21,24 +23,20 @@ def handler(event, context):
     Transforms metadata if needed
     Returns metadata
     """
-    
+
+    json_validate(event, upload_metadata_parser_event_schema)
+
+    # empty /tmp/ 
+    empty_tmp_folder()
 
     s3 = get_s3_client()
-
-    # Validate that the event matches the schema "source_bucket_schema"
-    json_validate(event, source_bucket_schema)
-
-    if isinstance(event, str):
-        event = json.loads(event)
     bucket = event["bucket"]
     zip_file = event["zip_file"]
 
-    print("opening s3 object")
-
     with open(COMMON_ZIP_PATH, "wb") as f:
         s3.download_fileobj(bucket, zip_file, f)
-
-    print("s3 object downloaded")
+    
+    print("Zip file downloaded from s3")
 
     source = Source(COMMON_ZIP_PATH)
     metadata_handler = source.get_metadata_handler()
@@ -46,25 +44,31 @@ def handler(event, context):
     # The correctly_structured handler is for where the metadata json should already
     # be in the shape we want
     if metadata_handler == MetadataHandler.correctly_structured.value:
+
+        print(f"Calling metadata handler for {MetadataHandler.correctly_structured.value}")
         with open(source.get_metadata_file_path()) as f:
             metadata_dict = json.load(f)
-            json_validate(metadata_dict, valid_metadata_schema)
-        
-    elif metadata_handler == MetadataHandler.some_other_structure.value:
-        log_as_incomplete()
-        raise NotImplementedError("Only looking at correctly structured metadata currently")
-        """
+            
+    # The cmd_csvw handler is for when the metadata json is in the format that 
+    # is created by CMD
+    elif metadata_handler == MetadataHandler.cmd_csvw.value:
+
+        print(f"Calling metadata handler for {MetadataHandler.cmd_csvw.value}")
         with open(source.get_metadata_file_path()) as f:
-            other_structure_metadata_dict = json.load(f)
+            cmd_csvw_metadata_dict = json.load(f)
             # do the transform of the metadata here, want it in the 
             # valid_metadata_schema format
-            metadata_dict = function(other_structure_metadata_dict)
-            json_validate(metadata_dict, valid_metadata_schema)
-        """
-
+            metadata_dict = cmd_csvw_metadata_parser(cmd_csvw_metadata_dict)
+            
     else:
+
         log_as_incomplete()
         raise ValueError(f"Unknown metadata handler {metadata_handler}")
 
+    
     log_as_complete()
-    return {"statusCode": 200, "body": metadata_dict}
+    return {
+        "statusCode": 200, 
+        "body": metadata_dict,
+        "request_id": event["request_id"]
+        }
